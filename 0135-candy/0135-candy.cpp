3342541b7339314403157f24339eb10183221179
MayankMT1024 <tmayank676@gmail.com>
2026-07-07 18:50:34 +0530

    Time: 0 ms (100%), Space: 22 MB (97.98%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int sum = 1, i = 1, n = ratings.size();
+        while(i < n){
+            while(i < n && ratings[i] == ratings[i-1]){
+                sum++;
+                i++;
+            }
+
+            int peak = 1;
+
+            while(i < n && ratings[i] > ratings[i-1]){
+                peak++;
+                sum += peak;
+                i++;
+            }
+
+            int down = 1;
+
+            while(i < n && ratings[i] < ratings[i-1]){
+                sum+=down;
+                down++;
+                i++;
+            }
+
+            if(down > peak) sum+=(down - peak);
+        }
+
+        return sum;
+    }
+};
