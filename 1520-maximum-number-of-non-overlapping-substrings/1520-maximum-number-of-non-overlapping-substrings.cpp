ef36c0105943be6eae2bfd7520690ee97423592d
MayankMT1024 <tmayank676@gmail.com>
2026-07-14 13:30:50 +0530

    Time: 9 ms (76.18%), Space: 23.2 MB (98.4%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -1,106 +1,46 @@
 class Solution {
 public:
     vector<string> maxNumOfSubstrings(string s) {
-        int first[26], last[26];
-        memset(first, -1, sizeof(first));
-        memset(last, -1, sizeof(last));
-        
+        vector<int> L(26, INT_MAX);
+        vector<int> R(26, -1);
         int n = s.length();
+        
         for (int i = 0; i < n; i++) {
-            if (first[s[i] - 'a'] == -1) {
-                first[s[i] - 'a'] = i;
-            }
-            last[s[i] - 'a'] = i;
+            L[s[i] - 'a'] = min(L[s[i] - 'a'], i);
+            R[s[i] - 'a'] = max(R[s[i] - 'a'], i);
         }
         
-        bool adj[26][26] = {false};
-        bool rev_adj[26][26] = {false};
+        vector<string> ans;
+        int last_right = -1;
         
-        for (int i = 0; i < 26; i++) {
-            if (first[i] != -1) {
-                for (int j = first[i]; j <= last[i]; j++) {
-                    int v = s[j] - 'a';
-                    if (i != v) {
-                        adj[i][v] = true;
-                        rev_adj[v][i] = true;
-                    }
-                }
-            }
-        }
-
-        //kosarajus algorith
-        
-        bool visited[26] = {false};
-        int stack[26];
-        int top = 0;
-
-        //step 1 finshing time
-        
-        auto dfs1 = [&](auto& self, int u) -> void {
-            visited[u] = true;
-            for (int v = 0; v < 26; v++) {
-                if (adj[u][v] && !visited[v]) {
-                    self(self, v);
-                }
-            }
-            stack[top++] = u;
-        };
-        
-        for (int i = 0; i < 26; i++) {
-            if (first[i] != -1 && !visited[i]) {
-                dfs1(dfs1, i);
-            }
-        }
-
-        //step 2 
-        
-        memset(visited, false, sizeof(visited));
-        int scc_id[26];
-        int curr_scc = 0;
-        
-        auto dfs2 = [&](auto& self, int u) -> void {
-            visited[u] = true;
-            scc_id[u] = curr_scc;
-            for (int v = 0; v < 26; v++) {
-                if (rev_adj[u][v] && !visited[v]) {
-                    self(self, v);
-                }
-            }
-        };
-        
-        for (int i = top - 1; i >= 0; i--) {
-            int u = stack[i];
-            if (!visited[u]) {
-                dfs2(dfs2, u);
-                curr_scc++;
-            }
-        }
-        
-        vector<bool> is_sink(curr_scc, true);
-        vector<int> scc_min_L(curr_scc, n + 1);
-        vector<int> scc_max_R(curr_scc, -1);
-        
-        for (int i = 0; i < 26; i++) {
-            if (first[i] != -1) {
-                int id = scc_id[i];
-                scc_min_L[id] = min(scc_min_L[id], first[i]);
-                scc_max_R[id] = max(scc_max_R[id], last[i]);
+        for (int i = 0; i < n; i++) {
+            if (i == L[s[i] - 'a']) {
+                int right = getValidRightBoundary(s, i, L, R);
                 
-                for (int v = 0; v < 26; v++) {
-                    if (adj[i][v] && scc_id[i] != scc_id[v]) {
-                        is_sink[id] = false;
+                if (right != -1) {
+                    if (i > last_right) {
+                        ans.push_back(""); 
                     }
+                    ans.back() = s.substr(i, right - i + 1);
+                    last_right = right;
                 }
             }
         }
         
-        vector<string> result;
-        for (int i = 0; i < curr_scc; i++) {
-            if (is_sink[i]) {
-                result.push_back(s.substr(scc_min_L[i], scc_max_R[i] - scc_min_L[i] + 1));
+        return ans;
+    }
+    
+private:
+    int getValidRightBoundary(const string& s, int i, const vector<int>& L, const vector<int>& R) {
+        int right = R[s[i] - 'a'];
+        
+        for (int j = i; j <= right; j++) {
+            if (L[s[j] - 'a'] < i) {
+                return -1; 
             }
+            right = max(right, R[s[j] - 'a']);
         }
         
-        return result;
+        return right;
     }
 };
