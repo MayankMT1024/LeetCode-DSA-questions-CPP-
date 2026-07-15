6b7e9cc450a5faa7f6332773757246621cad3746
MayankMT1024 <tmayank676@gmail.com>
2026-07-15 10:58:37 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.14%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 1) return 1;
+        int curr = 0, prev = 1, prev2 = 1;
+        for(int i = 2; i <= n; i++){
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+    }
+};
