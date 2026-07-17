6738785d098d4c56665059038881afacdc71e2b1
MayankMT1024 <tmayank676@gmail.com>
2026-07-17 13:15:32 +0530

    Time: 41 ms (77.26%), Space: 21.1 MB (28.88%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,41 +1,41 @@
 class Solution {
-    int memo[70][70][70];
-    int R, C;
-
-    int solve(vector<vector<int>>& grid, int r, int c1, int c2) {
-        if (c1 < 0 || c1 >= C || c2 < 0 || c2 >= C) {
-            return -1e9;
-        }
-        
-        if (r == R) {
-            return 0;
-        }
-        
-        if (memo[r][c1][c2] != -1) {
-            return memo[r][c1][c2];
-        }
-
-        int cherries = grid[r][c1];
-        if (c1 != c2) {
-            cherries += grid[r][c2];
-        }
-
-        int max_next = -1e9;
-        for (int dc1 = -1; dc1 <= 1; ++dc1) {
-            for (int dc2 = -1; dc2 <= 1; ++dc2) {
-                max_next = max(max_next, solve(grid, r + 1, c1 + dc1, c2 + dc2));
-            }
-        }
-
-        return memo[r][c1][c2] = cherries + max_next;
-    }
-
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        R = grid.size();
-        C = grid[0].size();
-        memset(memo, -1, sizeof(memo));
-        
-        return max(0, solve(grid, 0, 0, C - 1));
+        int R = grid.size();
+        int C = grid[0].size();
+        vector<vector<int>> dp(C, vector<int>(C, 0));
+
+        for (int r = R - 1; r >= 0; --r) {
+            vector<vector<int>> curr_dp(C, vector<int>(C, 0));
+            
+            for (int c1 = 0; c1 < C; ++c1) {
+                for (int c2 = 0; c2 < C; ++c2) {
+                    int cherries = grid[r][c1];
+                    if (c1 != c2) {
+                        cherries += grid[r][c2];
+                    }
+
+                    if (r == R - 1) {
+                        curr_dp[c1][c2] = cherries;
+                    } else {
+                        int max_next = 0;
+                        for (int dc1 = -1; dc1 <= 1; ++dc1) {
+                            for (int dc2 = -1; dc2 <= 1; ++dc2) {
+                                int nc1 = c1 + dc1;
+                                int nc2 = c2 + dc2;
+                                
+                                if (nc1 >= 0 && nc1 < C && nc2 >= 0 && nc2 < C) {
+                                    max_next = max(max_next, dp[nc1][nc2]);
+                                }
+                            }
+                        }
+                        curr_dp[c1][c2] = cherries + max_next;
+                    }
+                }
+            }
+            dp = curr_dp;
+        }
+
+        return dp[0][C - 1];
     }
 };
