e97e2f79892962a624c1ee9c417b1b445cc50df0
MayankMT1024 <tmayank676@gmail.com>
2026-07-18 22:50:22 +0530

    Time: 293 ms (85.31%), Space: 73.9 MB (26.68%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,32 +1,36 @@
 class Solution {
+    void generateSums(vector<int>& arr, int index, int count, int current_sum, vector<vector<int>>& sums) {
+        if (index == arr.size()) {
+            sums[count].push_back(current_sum);
+            return;
+        }
+        
+        generateSums(arr, index + 1, count + 1, current_sum + arr[index], sums);
+        
+        generateSums(arr, index + 1, count, current_sum, sums);
+    }
+
 public:
     int minimumDifference(vector<int>& nums) {
         int n = nums.size() / 2;
         int total_sum = 0;
+        
         for (int num : nums) {
             total_sum += num;
+        }
+
+        vector<int> left_half;
+        vector<int> right_half;
+        for (int i = 0; i < n; ++i) {
+            left_half.push_back(nums[i]);
+            right_half.push_back(nums[n + i]);
         }
 
         vector<vector<int>> left_sums(n + 1);
         vector<vector<int>> right_sums(n + 1);
 
-        int total_masks = 1 << n;
-        for (int mask = 0; mask < total_masks; ++mask) {
-            int left_sz = 0, left_val = 0;
-            int right_sz = 0, right_val = 0;
-            
-            for (int i = 0; i < n; ++i) {
-                if (mask & (1 << i)) {
-                    left_sz++;
-                    left_val += nums[i];
-                    
-                    right_sz++;
-                    right_val += nums[n + i];
-                }
-            }
-            left_sums[left_sz].push_back(left_val);
-            right_sums[right_sz].push_back(right_val);
-        }
+        generateSums(left_half, 0, 0, 0, left_sums);
+        generateSums(right_half, 0, 0, 0, right_sums);
 
         for (int i = 0; i <= n; ++i) {
             sort(right_sums[i].begin(), right_sums[i].end());
