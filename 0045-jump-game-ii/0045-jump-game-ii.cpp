5805ac88531a7a3685f08922383d7f123d3d2b7e
MayankMT1024 <tmayank676@gmail.com>
2026-07-30 16:05:50 +0530

    Time: 0 ms (100%), Space: 20.6 MB (74.51%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size(), l = 0, r = 0, jumps = 0;
+        while(r < n-1){
+            int maxi = 0;
+            for (int i = l; i <= r; ++i) maxi = max(maxi, i + nums[i]);
+            jumps++;
+            l = r + 1;
+            r = maxi;
+        }
+
+        return jumps;
+    }
+};
